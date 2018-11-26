018c53a15e413382afd448b488a832baf0482630
Reese Grimsley <reesul@tamu.edu>
2018-11-26 14:05:11 -0600

    Fixed indexing error for getLabel.m

--- Matlab/getLabel.m
+++ Matlab/getLabel.m
@@ -8,7 +8,7 @@ cellInd = strcmp(csvData(:,1), date);
 daysCells = csvData(cellInd,:);
 
 %find out which cell is actually correct
-for i = 1:length(daysCells)
+for i = 1:size(daysCells,1)
     startTime = date2num([daysCells{i,1}, ' ', daysCells{i,2}]);
     stopTime = date2num([daysCells{i,1}, ' ', daysCells{i,3}]);
     
